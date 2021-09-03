cc81c77e249eb6df9b5d9a93149436053205be64
정윤주 <jung-yunjoo@jeong-yunjuui-MacBookPro.local>
2021-09-03 17:16:35 +0900

    echo_mal

--- cmd/echo.c
+++ cmd/echo.c
@@ -80,7 +80,10 @@ void	cha_print(int i, t_env *env, char **cmd)
 	while (cmd[i][++j])
 		j = cha_cmd(&buf, cmd[i], env, j);
 	if (ft_strcmp(buf, "\0") == 0)
+	{
+		free(cmd[i]);
 		cmd[i] = NULL;
+	}
 	ft_strlcpy(cmd[i], buf, ft_strlen(buf));
 }
 
